78e1bdb76842869c49eedecc4083afd5cfb2e6ca
David11 <romerojesusdavid76@gmail.com>
2024-06-11 19:25:48 -0500

    curso en proceso

---
+++ tipos_de_datos/datos-simples.py
@@ -0,0 +1,27 @@
+"string" 
+'string'
+
+"""
+tus datos personales son:
+     nombre: jesus david
+     apellido: julio romero
+"""
+
+'''
+tus datos personales son:
+     nombre: jesus david
+     apellido: julio romero
+'''
+
+"""numeros enteros  = inteliger"""
+
+20
+26
+
+"""numeros flotantes = float"""
+34.5
+2009.4
+
+"""boleanos"""
+True
+False
